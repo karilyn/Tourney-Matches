b900ef2eeaeb5b8497f3eb80176f2a7cefd6c592
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-17 11:20:48 -0700

    Add props to all of the components

--- src/Components/MatchList.jsx
+++ src/Components/MatchList.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Match from "./Match";
+import matchData from "../data/matchData";
 
-function MatchList() {
+function MatchList(props) {
+  const oneMatch = matchData[0];
+
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      {/* Matches will be shown here */}
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
     </section>
   )
 }
--- src/Components/PlayerList.jsx
+++ src/Components/PlayerList.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Player from "./Player";
+import playerData from "../data/playerData";
+import matchData from "../data/matchData";
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
-function PlayerList() {
+function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+
+  const onePlayer = playerDataArray[0];
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      {/* Players will be shown here */}
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins}/>
     </section>
   )
 }
