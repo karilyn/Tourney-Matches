8b0954c87c425f8d8bdb47259cf965834176250d
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-03-17 11:21:56 -0700

    Import MatchList and PlayerList components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
 import './App.css';
+import MatchList from './components/MatchList';
+import PlayerList from './components/PlayerList';
+
 
 function App() {
   return (
     <div className="App">
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
